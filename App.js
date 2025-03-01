5524f6dc4a833ca1332ee89a1c867a5a1f389b34
lutherAlien <eyandilutherking2003@gmil.com>
2025-03-01 07:19:13 +0000

    add safearea view

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, SafeAreaView, Platform, StatusBar } from "react-native";
 import { NativeRouter, Route, Routes } from "react-router-native";
 import { HomeScreen } from "./screens/HomeScreen";
 import { AboutScreen } from "./screens/AboutScreen";
 
 export default function App() {
   return (
-    <NativeRouter>
-      <View style={styles.container}>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/about" element={<AboutScreen />} />
-        </Routes>
-      </View>
-    </NativeRouter>
+    <SafeAreaView style={styles.container}>
+      <NativeRouter>
+        <View style={styles.content}>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/about" element={<AboutScreen />} />
+          </Routes>
+        </View>
+      </NativeRouter>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40,
+    backgroundColor: "#f8f9fa",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
+  content: {
+    flex: 1,
+  }
 });
